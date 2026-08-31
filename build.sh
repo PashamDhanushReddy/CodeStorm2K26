faa54dba72be3b9b644a4a438576290c64845901
PashamDhanushReddy <pashamdhanushreddy@gmail.com>
2026-08-31 20:45:51 +0530

    deployement ready

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install --upgrade pip setuptools wheel
 
 # Install requirements
 pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
